420d8ce35b25227aa7d1bea7bce5fb6db415ed36
naveen199201 <naveen199201@gmail.com>
2024-04-30 18:48:10 +0530

    project upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./Signup";
+import Player from "./Player";
+import store from "./Store";
+import { Provider } from 'react-redux'
+import VideoUpload from "./VideoUpload";
+import Feed from "./Feed";
+import Profile from "./Profile";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import VideoDisplay from "./VideoDisplay";
+import SearchBar from "./SearchBar";
+
+const theme = createTheme(); // Create your Material-UI theme here
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/videodisplay" element={<VideoDisplay />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<AuthorizedRoute><Home /></AuthorizedRoute>} />
+            <Route path="/feed" element={<AuthorizedRoute><Feed /></AuthorizedRoute>} />
+            <Route path="/videos/:videoId/" element={<AuthorizedRoute><Player /></AuthorizedRoute>} />
+            <Route path="/profile" element={<AuthorizedRoute><Profile /></AuthorizedRoute>} />
+            <Route path="/upload" element={<AuthorizedRoute><VideoUpload /></AuthorizedRoute>} />
+            <Route path="/search" element={<AuthorizedRoute><SearchBar /></AuthorizedRoute>} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </Provider>
   );
 }
+
+const AuthorizedRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem('isAuthenticated');
+  if (!isAuthenticated) {
+    alert("Please login ");
+    return <Login />
+  }
+  return children;
+};
 
 export default App;
